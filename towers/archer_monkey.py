273cbdc4bad24a19ef826bed20c7226886238489
nokokiii <kubanokoki@gmail.com>
2022-10-23 16:35:41 +0200

    Archer Monkey imgs

--- towers/archer_monkey.py
+++ towers/archer_monkey.py
@@ -1,5 +1,25 @@
 import pygame
+from monkey_tower import Tower
 
+class ArcherMonkeyLong():
+    def __init__(self, x, y):
+        super().__init__(self, x, y)
+        self.tower_imgs = []
+        self.archer_imgs = []
+        self.archer_count = []
 
-class ArcherMonkey():
-    pass
+        # Load archer tower images
+        for x in range(1,4):
+            self.tower_imgs.append(pygame.transform.scale(pygame.image.load("game_asstes/towers/archer_long_" + str(x) + ".png"), (70, 70)))
+
+        for x in range(1,4):
+            self.tower_imgs.append(pygame.transform.scale(pygame.image.load("game_asstes/towers/archer_short_" + str(x) + ".png"), (70, 70)))
+    def draw(self, win):
+        super().draw(win)
+
+    def attack(self, enemies):
+        """
+        Attacks an enemy in the enemy list, modifies the list
+        :param enemies: list of enemies
+        :return: None
+        """
--- towers/monkey_tower.py
+++ towers/monkey_tower.py
@@ -15,19 +15,48 @@ class Tower:
         self.tower_imgs = []
 
     def draw(self, win):
-        pass
+        """
+        Draws the tower
+        :param win: surface
+        :return: None
+        """
+        img = (self.tower_imgs[self.level - 1])
+        win.blit(img, (self.x-img.get_width()//2, self.y-img.get_height()//2))
 
-    def draw(self):
-        pass
-
-    def click(self):
-        pass
+    def click(self, X, Y):
+        """
+        Returns if tower has been clicked on
+        and selects tower id it was clicked
+        :param X: int
+        :param Y: int
+        :return: Bool
+        """
+        if X <= self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
 
     def sell(self):
-        pass
+        """
+        Call to sell the tower, returns sell price
+        :return: int
+        """
+        return self.sell_price[self.level-1]
 
     def upgrade(self):
-        pass
+        """
+        Upgrades the tower for a given cost
+        :return: None
+        """
+        self.level += 1
 
-    def move(self):
-        pass
+    def get_upgrade_cost(self):
+        """
+        Returns the upgrade cost, if 0 then can't upgrade
+        anymore
+        :return: int
+        """
+        return self.price[self.level - 1]
+    def move(self, x, y):
+        self.x = x
+        self.y = y
