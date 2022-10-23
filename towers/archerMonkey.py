fe0ccd9d468167b84aab1635532e0cc7417d9395
nokokiii <kubanokoki@gmail.com>
2022-10-23 22:22:03 +0200

    Changed size of tower

--- towers/archerMonkey.py
+++ towers/archerMonkey.py
@@ -8,22 +8,32 @@ class ArcherMonkeyLong(Tower):
         self.tower_imgs = []
         self.archer_imgs = []
         self.archer_count = 0
+        self.range = 50
 
         self.tower_imgs.append(pygame.transform.scale(pygame.image.load("game_assets/towers/archer_long/archer_tower.png"), (70, 70)))
 
         # Load archer images
         for x in range(1, 5):
-            self.archer_imgs.append(pygame.transform.scale(pygame.image.load("game_assets/towers/archer_long/long_archer_" + str(x) + ".png"), (70, 70)))
+            self.archer_imgs.append(pygame.transform.scale(pygame.image.load("game_assets/towers/archer_long/long_archer_" + str(x) + ".png"), (55, 55)))
 
     def draw(self, win):
         super().draw(win)
-        if self.archer_count >= len(self.archer_imgs):
+        if self.archer_count >= len(self.archer_imgs) * 10:
             self.archer_count = 0
 
-        archer = self.archer_imgs[self.archer_count]
-        win.blit(archer, ((self.x + self.width/2) - (archer.get_width()/2), (self.y - archer.get_height())))
+        archer = self.archer_imgs[self.archer_count // 10]
+        win.blit(archer, ((self.x + self.width/2) - (archer.get_width()/2), (self.y - archer.get_height() + (archer.get_height()/2) - 5)))
 
         self.archer_count += 1
+
+    def change_range(self, r):
+        """
+        Change range of archer tower
+        :param r: int
+        :return: None
+        """
+        self.range = r
+
 
     def attack(self, enemies):
         """
